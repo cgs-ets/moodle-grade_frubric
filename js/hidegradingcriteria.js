77fbb35f637caac7b458df43a642a8f2eadee737
Veronica Bermegui <verisb@gmail.com>
2023-05-16 14:00:16 +1000

    Check for existance

--- js/hidegradingcriteria.js
+++ js/hidegradingcriteria.js
@@ -1,3 +1,5 @@
 // Once the student is graded, dont display the grading criteria in the submission status section
 
-document.querySelector('div.graded-form-h').closest('tr').style.display = 'none';
+if (document.querySelector('div.graded-form-h') != null) {
+    document.querySelector('div.graded-form-h').closest('tr').style.display = 'none';
+}
