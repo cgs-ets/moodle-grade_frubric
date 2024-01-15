7fa8a5a05447bc0b9fc01adf576fe2efadca5507
Michael Vangelovski <michael.vangelovski@hotmail.com>
2024-01-15 13:45:58 +1100

    Add effort into gradebook setup.

--- classes/task/cron_setup_gradebooks.php
+++ classes/task/cron_setup_gradebooks.php
@@ -50,17 +50,40 @@ class cron_setup_gradebooks extends \core\task\scheduled_task {
     public function execute() {
         global $DB;
         $requiredcats = ['FINAL', 'SEMESTER 1', 'SEMESTER 2', 'SEMESTER 3', 'CGS Effort', 'Classwork'];
-        $requiredgradeitems = [
-            '' => array(
+        $calculatedgradeitems = [
+            '' => array( //empty means insert these items at the root, rather than in a category.
                 'Semester 1 Grade|S1',
                 'Semester 2 Grade|S2',
                 'Semester 3 Grade|S3',
             )
         ];
-        $requiredrefs = [
+        $catrefs = [
             'SEMESTER 1' => 'S1',
             'SEMESTER 2' => 'S2',
             'SEMESTER 3' => 'S3',
+        ];
+        $effortgradeitems = [
+            'CGS Effort' => array( //empty means insert these items at the root, rather than in a category.
+                array('Term 1 Punctuality', 'Punctuality and Organisation includes prompt arrival to class, ensuring that you have all the correct equipment and that you respond to correspondence with staff.', 'T1P'),
+                array('Term 1 Classwork', 'Effective use of class time and technology includes working constructively, engaging in class discussions, listening well, taking notes, working collaboratively and utilising a mobile device effectively.', 'T1C'),
+                array('Term 1 Approach', 'Independent approach to learning emphasises self-discipline and active learning, for example, drafting work for peer/teacher feedback or persisting with tasks when concepts are challenging or reading more broadly on topics. You are responsible for your learning.', 'T1A'),
+                array('Term 1 Deadines', 'Meeting deadlines includes effective time management and thorough completion of homework and assignment tasks', 'T1D'),
+                
+                array('Term 2 Punctuality', 'Punctuality and Organisation includes prompt arrival to class, ensuring that you have all the correct equipment and that you respond to correspondence with staff.', 'T1P'),
+                array('Term 2 Classwork', 'Effective use of class time and technology includes working constructively, engaging in class discussions, listening well, taking notes, working collaboratively and utilising a mobile device effectively.', 'T1C'),
+                array('Term 2 Approach', 'Independent approach to learning emphasises self-discipline and active learning, for example, drafting work for peer/teacher feedback or persisting with tasks when concepts are challenging or reading more broadly on topics. You are responsible for your learning.', 'T1A'),
+                array('Term 2 Deadines', 'Meeting deadlines includes effective time management and thorough completion of homework and assignment tasks', 'T1D'),
+
+                array('Term 3 Punctuality', 'Punctuality and Organisation includes prompt arrival to class, ensuring that you have all the correct equipment and that you respond to correspondence with staff.', 'T1P'),
+                array('Term 3 Classwork', 'Effective use of class time and technology includes working constructively, engaging in class discussions, listening well, taking notes, working collaboratively and utilising a mobile device effectively.', 'T1C'),
+                array('Term 3 Approach', 'Independent approach to learning emphasises self-discipline and active learning, for example, drafting work for peer/teacher feedback or persisting with tasks when concepts are challenging or reading more broadly on topics. You are responsible for your learning.', 'T1A'),
+                array('Term 3 Deadines', 'Meeting deadlines includes effective time management and thorough completion of homework and assignment tasks', 'T1D'),
+                
+                array('Term 4 Punctuality', 'Punctuality and Organisation includes prompt arrival to class, ensuring that you have all the correct equipment and that you respond to correspondence with staff.', 'T1P'),
+                array('Term 4 Classwork', 'Effective use of class time and technology includes working constructively, engaging in class discussions, listening well, taking notes, working collaboratively and utilising a mobile device effectively.', 'T1C'),
+                array('Term 4 Approach', 'Independent approach to learning emphasises self-discipline and active learning, for example, drafting work for peer/teacher feedback or persisting with tasks when concepts are challenging or reading more broadly on topics. You are responsible for your learning.', 'T1A'),
+                array('Term 4 Deadines', 'Meeting deadlines includes effective time management and thorough completion of homework and assignment tasks', 'T1D'),
+            )
         ];
 
         // Get courses under Senior Academic
@@ -104,8 +127,8 @@ class cron_setup_gradebooks extends \core\task\scheduled_task {
             }
 
             // Creating category causes new entries into the gradeitem table too.
-            // Add ID numbers to the category grade items so that it can be referenced in the formulas later.
-            foreach ($requiredrefs as $catname => $requiredref) {
+            // Add ID numbers to the category grade items so that they can be referenced in the formulas later.
+            foreach ($catrefs as $catname => $ref) {
                 // Get the category.
                 $cat = \grade_category::fetch(array(
                     'courseid' => $course->id, 
@@ -121,15 +144,15 @@ class cron_setup_gradebooks extends \core\task\scheduled_task {
                     $this->log("Grade item for category not found: $catname");
                     continue;
                 }
-                if ($gradeitem->idnumber != $requiredref) {
-                    $this->log("Updating idnumber (for formula reference) for grade item $gradeitem->id to $requiredref");
-                    $gradeitem->idnumber = $requiredref;
+                if ($gradeitem->idnumber != $ref) {
+                    $this->log("Updating idnumber (for formula reference) for grade item $gradeitem->id to $ref");
+                    $gradeitem->idnumber = $ref;
                     $gradeitem->update();
                 }
             }
 
-            // Create requiredgrade items.
-            foreach ($requiredgradeitems as $catname => $catgradeitems) {
+            // Create calculated grade items.
+            foreach ($calculatedgradeitems as $catname => $catgradeitems) {
                 foreach ($catgradeitems as $gradeitemname) {
                     list($gradeitemname, $refidnumber) = explode('|', $gradeitemname);
                     // Get the category where to insert this grade item. Empty means root.
@@ -179,6 +202,57 @@ class cron_setup_gradebooks extends \core\task\scheduled_task {
                         'grademax' => 6,
                         'grademin' => 1,
                         'calculation' => $calculation,
+                    );
+                    $gradeitem = new \grade_item($params, false);
+                    $gradeitemid = $gradeitem->insert();
+                }
+            }
+
+            // Create effort grade items.
+            foreach ($effortgradeitems as $catname => $catgradeitems) {
+                foreach ($catgradeitems as $gradeitem) {
+                    list($name, $info, $idnumber) = $gradeitem;
+                    // Get the category where to insert this grade item. Empty means root.
+                    $cat = null;
+                    if (empty($catname)) {
+                        $cat = \grade_category::fetch(array(
+                            'courseid' => $course->id, 
+                            'fullname' => '?', 
+                            'depth' => 1, 
+                        ));
+                    } else {
+                        $cat = \grade_category::fetch(array(
+                            'courseid' => $course->id, 
+                            'fullname' => $catname, 
+                        ));
+                    }
+                    if (!$cat) {
+                        $this->log("Issue fetching cat: $catname");
+                        continue;
+                    }
+                    
+                    // Check if the grade item already exists for the course.
+                    $gradeitem = \grade_item::fetch(array('courseid'=> $course->id, 'itemname'=> $name));
+                    if ($gradeitem) {
+                        $this->log("Grade item already exists: $name");
+                        continue;
+                    }
+
+                    // Get the effort scale.
+                    $scale = $DB->get_record('scale', array('courseid' => 0, 'name' => 'Effort rubric scale'));
+   
+                    $this->log("Adding grade item '$name' to category '$cat->id'");
+                    $params = array(
+                        'itemtype'  => 'manual',
+                        'iteminfo'  => $info,
+                        'idnumber'  => $idnumber,
+                        'itemname'  => $name,
+                        'gradetype' => GRADE_TYPE_SCALE,
+                        'courseid'  => $course->id,
+                        'categoryid' => $cat->id,
+                        'grademax' => 5,
+                        'grademin' => 1,
+                        'scaleid' => $scale ? $scale->id : null,
                     );
                     $gradeitem = new \grade_item($params, false);
                     $gradeitemid = $gradeitem->insert();
